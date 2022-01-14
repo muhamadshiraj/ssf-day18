60187cf6edafff5268534aaec25095f1df980d0a
Shiraj <muhamadshiraj@gmail.com>
2022-01-14 10:33:09 +0800

    this is the first commit

---
+++ src/main/java/ssf/day18/WeathermapApplication/WeathermapApplication.java
@@ -0,0 +1,13 @@
+package ssf.day18.WeathermapApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeathermapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeathermapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ssf/day18/WeathermapApplication/WeathermapApplicationTests.java
@@ -0,0 +1,13 @@
+package ssf.day18.WeathermapApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeathermapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
